05b1195039a66ed623c8acfdcd6c27980860710a
jose luis <cortezluisjose@gmail.com>
2019-07-29 03:34:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,4 +10,24 @@ def sort_array_desc
   
   
 end
-def s
+def sort_array_char_count
+  
+end
+def swap_elements
+  
+end
+def reverse_array
+  
+end
+def kesha_maker
+  
+end
+deffind_a
+
+end
+def sum_array
+  
+end
+def add_s
+  
+end
